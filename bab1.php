145e9c0f4dc8b0f72b1a07ea480e32836604b14b
unknown <roi.hanifroihan@gmail.com>
2025-10-07 21:09:56 +0700

    Membuat isi dari bab1 lanjutan

--- bab1.php
+++ bab1.php
@@ -46,7 +46,7 @@
                     <li>Meningkatkan kualitas pelayanan publik di bidang statistik pada Instansi Pusat dan Pemerintahan Daerah.</li>
                 </ul>
             </div>
-
+            
             <hr class="info-divider">
 
             <div class="info-section">
@@ -62,9 +62,52 @@
                     </div>
                 </div>
             </div>
-             <!-- <a href="form-input.php" class="cta-button">Lanjut Isi Form Bab I</a> -->
+
+            <hr class="info-divider">
+
+            <div class="info-section">
+                <h3>BAGAIMANA EPSS DILAKUKAN?</h3>
+                <ul>
+                    <li><b>Penilaian Mandiri</b> dengan menilai tingkat kematangan dari masing-masing indikator, memberikan penjelasan yang diperlukan, dan menyampaikan bukti dukung oleh Tim Penilai Internal.</li>
+                    <li><b>Penilaian Dokumen</b> dengan verifikasi tingkat kematangan hasil penilaian mandiri berdasarkan bukti dukung yang disampaikan oleh Tim Penilai Badan.</li>
+                    <li><b>Penilaian Interviu</b> yang dilakukan dengan metode tanya jawab antara Tim Penilai Badan dengan Tim Penilai Internal untuk meyakinkan validitas bukti dukung.</li>
+                    <li><b>Penilaian Visitasi</b> yang dilakukan langsung oleh Tim Penilai Badan untuk validasi hasil penilaian mandiri.</li>
+                </ul>
+            </div>
+
+            <hr class="info-divider">
+
+            <div class="info-section">
+                <h3>SIAPA YANG TERLIBAT DALAM EPSS?</h3>
+                <ul>
+                    <li>Tim Penilai Internal (K/L/Pemda)</li>
+                    <li>Tim Penilai Badan (BPS Pusat/Prov/Kab/Kota)</li>
+                    <li>Tim Penjaminan Kualitas (BPS Pusat/Prov)</li>
+                    <li>Tim Pelaksana (BPS Pusat/Prov/Kab/Kota)</li>
+                </ul>
+            </div>
+
+            <hr class="info-divider">
+
+            <div class="info-section">
+                <h3>KAPAN EPSS DILAKUKAN?</h3>
+                <p>EPSS dilakukan setiap 2 tahun sekali, atau sewaktu-waktu sesuai kebutuhan.</p>
+            </div>
+
+            <hr class="info-divider">
+
+            <div class="info-section">
+                <h3>OUTPUT DARI EPSS?</h3>
+                <p><b>Indeks Pembangunan Statistik (IPS)</b>. Hasil EPSS dapat dibagipakaikan, utamanya kepada:</p>
+                <ul>
+                    <li><b>Kemendagri:</b> Sebagai bahan penyusunan IKU OPD.</li>
+                    <li><b>KemenPAN-RB:</b> Sebagai indikator nilai RB General.</li>
+                    <li><b>KemenPPN/Bappenas:</b> Sebagai bahan evaluasi progres SDI dan bahan penyusunan RPJMN.</li>
+                </ul>
+            </div>
+            <a href="pembinaan.php" class="cta-button">Kembali</a>
         </div>
-        </div>
+    </div>
 
     <footer class="footer">
         <p>&copy; 2024 Badan Pusat Statistik. Hak Cipta Dilindungi.</p>
